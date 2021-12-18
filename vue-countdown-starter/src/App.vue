ceb29f95039e931655645c86bb67ffafa4a2efd9
James Phillips <james@moresoda.co.uk>
2021-12-18 15:27:55 +0000

    Installed npm

--- vue-countdown-starter/src/App.vue
+++ vue-countdown-starter/src/App.vue
@@ -1,4 +1,7 @@
-<template></template>
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
 
 <script>
 // const eventData = [
@@ -46,8 +49,23 @@
 //   },
 // ];
 
-export default {};
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
-<style >
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
