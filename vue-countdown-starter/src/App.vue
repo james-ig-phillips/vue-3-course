9ef5dc372cc132cb03f015d9a86b712f55654f79
James Phillips <james@moresoda.co.uk>
2021-12-18 21:20:32 +0000

    more npm stuff

--- vue-countdown-starter/src/App.vue
+++ vue-countdown-starter/src/App.vue
@@ -1,71 +1,109 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<form action="">
+		<p>add new event</p>
+	</form>
+    <div class="options">
+      <button @click="showPastEvents = !showPastEvents">show past events</button>
+    </div>
+    <ul>
+        <li v-for="event in orderEvents" :key="event.id">
+            <Event
+             :event="event" 
+             :daysLeft="daysLeft(event)" 
+             :showPastEvents="showPastEvents"/>
+        </li>
+    </ul>
+
 </template>
 
 <script>
-// const eventData = [
-//   {
-//     id: 1,
-//     name: "Graduation",
-//     details: "wooohoo!!!",
-//     date: "2020-09-25",
-//     background: "#F34949",
-//   },
-//   {
-//     id: 2,
-//     name: "Holidays",
-//     details: "wooohoo!!!",
-//     date: "2021-12-30",
-//     background: "#f9f970",
-//   },
-//   {
-//     id: 3,
-//     name: "HolidayYYYY",
-//     details: "Get me outta here!",
-//     date: "2020-09-12",
-//     background: "#7AD3F0",
-//   },
-//   {
-//     id: 4,
-//     name: "Birthday",
-//     details: "My birthday party",
-//     date: "2020-10-02",
-//     background: "#F07AEC",
-//   },
-//   {
-//     id: 5,
-//     name: "Christmas",
-//     details: "ho ho ho",
-//     date: "2020-03-5",
-//     background: "#EB9A0F",
-//   },
-//   {
-//     id: 6,
-//     name: "Conference Talk",
-//     details: "dont flop",
-//     date: "2021-02-28",
-//     background: "#68EE94",
-//   },
-// ];
+const eventData = [
+  {
+    id: 1,
+    name: "Graduation",
+    details: "wooohoo!!!",
+    date: "2021-09-25",
+    background: "#F34949",
+  },
+  {
+    id: 2,
+    name: "Holidays",
+    details: "wooohoo!!!",
+    date: "2022-12-30",
+    background: "#f9f970",
+  },
+  {
+    id: 3,
+    name: "HolidayYYYY",
+    details: "Get me outta here!",
+    date: "2021-12-18",
+    background: "#7AD3F0",
+  },
+  {
+    id: 4,
+    name: "Birthday",
+    details: "My birthday party",
+    date: "2021-12-19",
+    background: "#F07AEC",
+  },
+  {
+    id: 5,
+    name: "Christmas",
+    details: "ho ho ho",
+    date: "2021-12-25",
+    background: "#EB9A0F",
+  },
+  {
+    id: 6,
+    name: "Conference Talk",
+    details: "dont flop",
+    date: "2022-02-28",
+    background: "#68EE94",
+  },
+];
 
-import HelloWorld from './components/HelloWorld.vue'
+import Event from './components/Event.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        Event
+    },
+    data() {
+        return {
+            events: eventData,
+            showPastEvents: true,
+        }
+    },
+    methods: {
+        daysLeft(event) {
+            const Time = Date.parse(event.date) - Date.now();
+            const Days = Math.ceil(Time / (1000 * 3600 * 24));
+            return Days;
+        }
+    },
+    computed: {
+        orderEvents() {
+			const eventsToOrder = this.events
+			return eventsToOrder.sort((a, b) => a.date > b.date ? 1 : -1);
+		}
+    }
 }
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+ul {
+    padding: 0;
+}
+
+li {
+    list-style: none;
+    cursor: pointer;
 }
 </style>
